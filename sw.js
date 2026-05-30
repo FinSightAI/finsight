17196420308bd7821010ac31d53dabd996b31e19
FinSight <ofirshamir57@gmail.com>
2026-05-30 18:54:23 -0300

    chore: bump SW versions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finsight-v334';
+const CACHE_NAME = 'finsight-v335';
 
 // Listen for "user clicked Update" message — activate immediately
 self.addEventListener('message', e => {
